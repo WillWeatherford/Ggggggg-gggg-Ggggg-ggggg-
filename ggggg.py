29ff23b273ab870586140c676fa5d77a4066cc60
Will Weatherford <weatherford.william@gmail.com>
2015-12-22 00:15:48 -0800

    Huffman encode and decode working!

--- ggggg.py
+++ ggggg.py
@@ -6,86 +6,60 @@ MIN_CODEBIT_LEN = 2
 BIT = ('g', 'G')
 
 
-class Node(object):
-    def __init__(self):
-        self.bits = ''
-        self.parent = None
-
-    def assign_bits(self):
-        if not hasattr(self, 'children'):
-            return
-        for i, c in enumerate(self.children):
-            c.bits = self.bits + BIT[i]
-            c.assign_bits()
-
-    def get_code(self):
-        if not hasattr(self, 'children'):
-            return {self.char: self.bits}
-        else:
-            code = {}
-            for c in self.children:
-                code.update(c.get_code())
-            return code
-
-
-class InnerNode(Node):
+class InnerNode(object):
     def __init__(self, children):
-        super(InnerNode, self).__init__()
         self.children = children
-        for c in children:
-            c.parent = self
-        print('InnerNode created. children:\n\t{}'.format('\n\t'.join(
-            [str(c) for c in self.children])))
+        self.bits = ''
+        # print('InnerNode created. children:\n\t{}'.format('\n\t'.join(
+        #     [str(c) for c in self.children])))
 
     @property
     def count(self):
         return sum([c.count for c in self.children])
 
+    def get_code(self):
+        code = {}
+        children = sorted(self.children, key=lambda c: c.count, reverse=True)
+        for i, c in enumerate(children):
+            c.bits = self.bits + BIT[i]
+            code.update(c.get_code())
+        return code
 
-class CharNode(Node):
+
+class CharNode(object):
     def __init__(self, char, count):
-        super(CharNode, self).__init__()
         self.char = char
         self.count = count
+        self.bits = ''
 
     def __str__(self):
         return 'Char "{}"; count: {}, bits: {}'.format(self.char, self.count,
                                                        self.bits)
 
-
-def char_count(s):
-    chars = list(set(string.ascii_letters) & set(s))
-    chars = [CharNode(char, s.count(char)) for char in chars]
-    return chars
+    def get_code(self):
+        return {self.char: self.bits}
 
 
 def encode(s):
-    # count num of used chars, i.e. minimum amount of codebits needed
-    chars = char_count(s)
-    queue = list(chars)
+    chars = list(set(string.ascii_letters) & set(s))
+    queue = [CharNode(char, s.count(char)) for char in chars]
+
     while len(queue) > 1:
-        print('Re-sorting queue...')
         queue = sorted(queue, key=lambda c: c.count)
-        print('Queue re-sorted. Length: {}'.format(len(queue)))
-        newnode = InnerNode((queue.pop(0), queue.pop(0)))  # get first 2 (lowest count) from queue
+        newnode = InnerNode((queue.pop(0), queue.pop(0)))
         queue.append(newnode)
-    print('Exited while loop.')
     root = queue.pop()
-    root.assign_bits()  # combine
-    code = root.get_code()  # into one func
-    print(code)
+    code = root.get_code()
+
     keystring = ' '.join([' '.join(pair) for pair in code.items()])
-    for char, codebit in code.items():
-        s = s.replace(char, codebit)
-    return '\n'.join((keystring, s))
+    encoded = ''.join([code[c] if c.isalpha() else c for c in s])
+    return '\n'.join((keystring, encoded))
 
 
 def decode(s):
     matches = list(KEY_RE.finditer(s))
-    assert matches, 'Unable to match decoding key regular expression.'
 
     keys = dict([reversed(m.group().strip(' \n').split(' ')) for m in matches])
-    assert keys, 'Unable to create decoding keys.'
     keys_re = re.compile(r'' + '|'.join([r'^' + k for k in keys.keys()]))
 
     last_match_end = matches[-1].end()
@@ -96,7 +70,9 @@ def decode(s):
         if s[0].isalpha():
             match = keys_re.match(s)
             if not match:
-                raise TypeError('Unable to match a decode key pattern at start of string.')
+                raise TypeError('Unable to match a decode key pattern at start '
+                                'of string. Decoded so far:\n{}'.format(
+                                    ''.join(letters)))
             end_index = match.end()
         else:
             end_index = 1
