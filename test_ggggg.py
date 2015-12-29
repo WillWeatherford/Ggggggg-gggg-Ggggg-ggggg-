ec0a9257ffa6610ebe47ccff297c4a47a52b871e
Will Weatherford <weatherford.william@gmail.com>
2015-12-28 22:23:46 -0800

    Added test file and instructions to repo

---
+++ test_ggggg.py
@@ -0,0 +1,74 @@
+import ggggg
+
+
+s1 = 'H GgG d gGg e ggG l GGg o gGG r Ggg w ggg GgGggGGGgGGggGG, ggggGGGggGGggGg!'
+s2 = 'a GgG d GggGg e GggGG g GGGgg h GGGgG i GGGGg l GGGGG m ggg o GGg p Gggg r gG y ggG GGGgGGGgGGggGGgGggG /gG/GggGgGgGGGGGgGGGGGggGGggggGGGgGGGgggGGgGggggggGggGGgG!'
+s3 = '''
+C GgggGgg H GgggGgG T GgggGGg a gGg c GGggG d GggG e GgG g ggGgG
+h GGgGg i gGGg j GgggGGG l gGGG m ggGGg n GGgGG o ggg p ggGGG r GGGg
+s GGGG t GGgggG u ggGgg v Ggggg w GGggggg y GGggggG
+GgggGGgGGgGggGGgGGGG GGggGGGgGggGggGGGgGGGGgGGGgGGggGgGGgG
+GGggggggGgGGGG ggGGGGGGggggggGGGgggGGGGGgGGggG gGgGGgGGGggG
+GggGgGGgGGGGGGggGggGggGGGGGGGGGgGGggG gggGggggGgGGGGg gGgGGgggG
+/GGGg/GggGgGggGGggGGGGGggggGggGGGGGGggggggGgGGGGggGgggGGgggGGgGgGGGGg_gGGgGggGGgGgGgGGGG.
+GgggGgGgGgGggggGgG gGg GGggGgggggggGGG
+GGggGGGgGggGggGGGgGGGGgGGGgGGggGgGGgG gGGgGggGGgGgGg?
+GgggGgggggggGGgGgG GgggGGGggggGGgGGgGG ggGggGGGG gggGggggGgGGGGg
+GGgggGGGgGgGgGGGGgGgG!
+'''
+s4 = '''
+A gGgGgGgG B gGgGgGggGg C gGgGgGggGG H GGgGgGgggg I GggGggg L GGgGgGgggG N GGgGgGggGg S GGgGgGggGG T GGgGgGgGg W gGgGgGggg Y gGgGgGGg a GGGG b gGgGgg c gGgg d gGgGG e GGGg f GggGgG g ggggg h gggG i Gggg j GGgGgGG k GGgGgg l GGgGG m ggggG n gGGG o ggG p GggGggG r gGGg s GGgg t GgGg u GgGGG v gGgGgGGG w GgGGg x GGgGgGgGG y GggGG
+GGgGgGggggGGGggGGgGGGg'GGgg GgGggggGGGGg GgGggggGGggggGGGggggg. gGgGgGGgggGGgGGG GGggGGGGGggggGgGG GGGG "GGgGgGGGGGGgGggGGgGgggGgGGGGGGGgGGg GgggGGgg GGGG gGgggGGgggGGgGGg."
+GggGgggGGgg GgggGgGg GggggGGG GgGggggGGGGg GGggGGGGggggGGGGg GggGgGGGGGggggGGgggGGgGGGggGG? gGgGgGGgGGGgGGgg. GGgGgGggGgggG ggGgGGGGGGg'GGgg GGGGgGGggggggGgGGGGggggGGGggggg GgGggggGGGGGGgGg.
+gGgGgGgGGGgg GGggggGggggGGGGgggGgGGGGGGg GgGGggggGggG GgggGGgg GGGG GGgggGggGgggGGGggGGGGgGgGgggGGggGgGg GgGGggggGggG GGggGgGgGgGGGgGgGGGgggGGGgGGgg gGgggGGgggGGgGGgGGgg, GggGggg GGGGggggG GgGgGGGgGGgGGGGgGGGggggGGGggggg GggGGggGGgGGG, GGggGggGggGGGGggGggGgggGggGgGGggggGggGGGGGGgGGGGgGGGggGG, GggggGGG GGgggGggGgggGGGggGGGgGggGGGg, gGGGggG ggGgGGGGGGg gGggGGGGGGgGGGGgGGGGgg GGgGgGGGGGGgGggGGgGgggGgGGGGGGGgGGgGGgg gGgggGGgggGGgGGgGGgg. GggGgggGggGgG GggGGggGGgGGG GgGGgGGGGgGGGGgGg GgGgggG gGgGggGGGg "GGggGggGggGGGGggGggGgggGggGgGGggggGgg" GGgGGGgggGGgGggGGGg GggGGggGGgGGG GGggGGGGGggggGgGG, GgGggggGGGGggGGG GggGGggGGgGGG GGgggggGggGGgGGGGGgGGgGgGGgGGG'GgGg GGGgGgggGgGggggGGGGggGGg. GGgGgGgGggggGGGGgGggGG'gGGgGGGg gGGGggGGgGg GgGggggGGGGg GGggGGGGggggGGGGg GgGggggGGggggGGGggggg.
+GggGgggGggGgG GggGGggGGgGGG'gGGgGGGg GGggGGGGGggGGGggggGGGggggg "gGgggGGgggGGgGGg GggGgGGGGGggggGGgggGGgGGGggGG" GggGGggGGgGGG'gGGgGGGg gGGgGGGgGggGgGGGGggGGggGGgGggggGGGggggg GgGgggG GgGggggGGGGg GgGgGGGGGGgGgGgGGggGgGGGggGggggGGggggGgg ggggggGGgggGGgGGGGggGggGGggggGGGggggg ggGGggGgG gGgGgGggGGggGgGGggGgGgGGGGggggGgGGGGGGGGGg, GgGGggggGGggggGgggggG GggggGGGgGggGGgGGGgGGGgGgGGGGGgGGgg GgGggggGGggggGGGgggggGGgg GggGgGgGGgggGggggG gGGGGgGGGGgGggGgggGGgGGGGgGggGGgGggGGGggGGgGGgg GgGgggG gGgGggGGgGGGgGGGGGGg GGgGgGGGGGGGggGGGGgg GgGgggG gGGgGGGGgGgGgGGGGGGggGGGGGgg.
+GGgGgGggGGggG GggGGggGGgGGGgGGg gGGgGGGgGGGGGGggggGgGGGGggggGGGggggg GggGgGggGgGGg gGggGGGGGGgGGGGgGGGggggGGGggggg GGGG GGgGgGGGGGGgGggGGgGgggGgGGGGGGGgGGg GGGG gGgggGGgggGGgGGg GgggGGgg gGgGggGGGggGggGGGGGgGGGGGggGGGg gGGgGGGGgGGGgGgGGggGggggG GggGggGGGGgggGGggGggGGGgGGGGGg "gGggGGGGGGgGGGGgGG GgGggggGGGGg gGgGggGGgGGGGGGgGggGGgGgg ggGgGGGGGGgGGgg gGgggGGgggGGgGGgGGgg?" GGgGgGgggGGGGgGgGg'GGgg gggggGGGgGgGg ggggggGGgGGGGgGggGGgGggGGgGGGGGgGGgg GGGGgGGGgGgGG gGgGggGGgGGGGGGgGggGGgGgggGgGggGggggGGggGgGGGGgg GggggGGG GgGggggGGGGggGGgGGGg, GgGggggGGGGggGGG, GgGgggGggG.
+gGgGgGgGGGgGGGGggggG, gGggGGGGGGgGGGGgGGGggggGGGggggg GGggggGggggGGGGgggGgGGGGGGg GGGG gggGGgGGGggggGGGGGgGGG ggGgGGg GGGGgGGG GGGGGggGggGGGGg? GggGgggGgGg'GGgg gGGGggGGgGg ggGgGGGGGGg ggGgGGg GgGggggGGGGg ggGGgGggggGGGGggGGg, GgGggggGGGGGGgGg'GGgg gGGGggGGgGg gggGggGGgGGg GgGgGGGGGGgGgGgGGggGgGGGggGggggGGggGG GgGGgggGgGGgGGgGggGGgg. GGgGgGgGggggGGGGgGggGG'gGGgGGGg gGgGggggGGgGggggG. gGgGgGgG GGgGgGGGGGGgGggGGgGgggGgGGGGGGGgGGg GgggGGgg GGGG GGgGgGGGGGGgGggGGgGgggGgGGGGGGGgGGg GGGGgGGGgGgGG GGGG ggggGGGGgggggGgGgGggGGGggGGg ggGGggGgG GgGggggGGGGg gGgggGGgggGGgGGg GggGgGGGGGggggGGgggGGgGGGggGG. gGgGgGggGgGgGGGGgGg GgGggggGGGGGGgGg'GGgg gGGGggGGgGg GgGGggggGGGGGGgGg GggGGggGGgGGG GGggGGGGGggggGgGG. gGgGgGGgggGGgGGG GGggGGGGGggggGgGG GGGG GGgGgGGGGGGgGggGGgGgggGgGGGGGGGgGGg GgggGGgg GGGG gGgggGGgggGGgGGg, GgGGggggGGggggGgggggG GgggGGgg gGGGggGGgGg GgGggGGgGgGGGGGGg GgGGGgGGGGGgGGGGGgGGggGGgg GggGGggGGgGGG'gGGgGGGg ggGGGgGggGGGGGggGG GgGGgGgggGgGggggG gGggGGGGGGgGGGGgGGGggggGGGggggg GGGGGGgGGGGgGG ggggGGGGgggggGgGgGggGGGggGGgGGgg ggGGggGgG GgGggggGGGGg gGgggGGgggGGgGGg GggGgGGGGGggggGGgggGGgGGGggGG gGgggGGgggGGgGGgGGgg, GgGGggggGGggggGgggggG ggggGGGGgGGGGgGGGGGgg GggGGggGGgGGG'gGgGG gGggGGGGGGgGGGGgGG gGgGggGGgGGGgGGGGGGg GGgGgGGGGGGGggGGGGgg, gGGgGGGGgGgGgGGGGGGggGGGGGgg, GGGGgGGGgGgGG ggGGgGggggGGGGggGGg gGgGggGggggGGggGgGGGGgg gGgggGGgggGGgGGgGGgg, GgGgggGggG. gGgGgGggggggGGggggGgggggG GggGGggGGgGGG GGggGGGGGggggGgGG GggGGggGGgGGG gGgGGggGgGGG'GgGg.
+GggGgggGgGg'GGgg ggGGGgGggGGGGGggGG GgGgggG GGgGgGGGgGGGGGggGgGg GGGGgGgGGggggGGgggGgGg GggGGggGGgGGG'gGGgGGGg GgGGggGGgggGgGGGggggg, GggGGggGGgGGG GGgGgggGGGggGGgGGg?
+'''
+s4_answer = '''
+Here's the thing. You said a "jackdaw is a crow."
+Is it in the same family? Yes. No one's arguing that.
+As someone who is a scientist who studies crows, I am telling you, specifically, in science, no one calls jackdaws crows. If you want to be "specific" like you said, then you shouldn't either. They're not the same thing.
+If you're saying "crow family" you're referring to the taxonomic grouping of Corvidae, which includes things from nutcrackers to blue jays to ravens.
+So your reasoning for calling a jackdaw a crow is because random people "call the black ones crows?" Let's get grackles and blackbirds in there, then, too.
+Also, calling someone a human or an ape? It's not one or the other, that's not how taxonomy works. They're both. A jackdaw is a jackdaw and a member of the crow family. But that's not what you said. You said a jackdaw is a crow, which is not true unless you're okay with calling all members of the crow family crows, which means you'd call blue jays, ravens, and other birds crows, too. Which you said you don't.
+It's okay to just admit you're wrong, you know?
+'''
+
+
+def test_decode1():
+    assert ggggg.decode(s1) == 'Hello, world!'
+
+
+def test_decode2():
+    assert ggggg.decode(s2) == 'hooray /r/dailyprogrammer!'
+
+
+def test_decode3():
+    print(ggggg.decode(s3))
+    return True
+
+
+def test_decode4():
+    assert ggggg.decode(s4) == s4_answer.strip(' \n')
+
+
+def test_both():
+    test_strings = [
+        'Hello world!',
+        'hooray /r/dailyprogrammer!',
+        'this is an example of a huffman tree',
+        "j'aime aller sur le bord de l'eau les jeudis ou les jours impairs",
+        s4_answer
+    ]
+    for s in test_strings:
+        encoded = ggggg.encode(s)
+        print(encoded)
+        decoded = ggggg.decode(encoded)
+        print(decoded)
+        assert(decoded == s.strip(' \n'))
+
+# if __name__ == '__main__':
+#     print(ggggg.encode('Hello world!'))
+#     print(ggggg.encode('this is an example of a huffman tree'))
